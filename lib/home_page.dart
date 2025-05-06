5f5d8f7dfd94467e8708da10bef78923ab9ad982
J�lia Rodrigues Siqueira <48727174816@>
2025-05-06 11:34:34 -0300

    login

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -17,20 +17,93 @@ class _HomePageState extends State<HomePage>{
         child: Column(
           children: [
             Padding(padding: EdgeInsets.only(top: 50), child:  Image.asset("assets/images/title.png", width: 350)),
-            Text('Login', style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600, color: Colors.brown)),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Username',
-                border: OutlineInputBorder()
-              ),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              decoration: InputDecoration(
-                  hintText: 'Password',
-                  border: OutlineInputBorder()
-              ),
-            ),
+            Padding(padding: EdgeInsets.all(20), child:
+              Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Username',
+                        filled: true,
+                        fillColor: Color(0xFFb69889),
+                        hintStyle: TextStyle(color: Colors.white),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide(
+                                color: Colors.brown,
+                                width: 3
+                            )
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide(
+                                color: Colors.brown,
+                                width: 3
+                            )
+                        ),
+
+                        disabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide(
+                                color: Colors.brown,
+                                width: 3
+                            )
+                        ),
+
+
+
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      filled: true,
+                      fillColor: Color(0xFFb69889),
+                      hintStyle: TextStyle(color: Colors.white),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                              color: Colors.brown,
+                              width: 3
+                          )
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                              color: Colors.brown,
+                              width: 3
+                          )
+                      ),
+
+                      disabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                              color: Colors.brown,
+                              width: 3
+                          )
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                      onPressed: (){print('object');} ,
+                      child: Text('Login'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.brown,
+                        foregroundColor: Colors.white,
+                        padding: EdgeInsets.all(10),
+                        textStyle: TextStyle(fontSize: 23),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30)
+                        ),
+                        minimumSize: Size(375, 60)
+                      ),
+
+                  ),
+
+                ],
+              ),)
+
 
 
             ]
